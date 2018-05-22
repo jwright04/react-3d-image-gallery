d7ad0d32e85e342a19d1375c793cd20c27a97d69
James Wright <jamwright@admins-MacBook-Pro-5.local>
2018-05-22 12:06:52 -0500

    first commit

---
+++ components/Canvas.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {asset, Pano} from 'react-vr';
+
+class Canvas extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			src: this.props.src,
+		}
+	}
+
+	componentWillReceiveProps(nextProps){
+		console.log('nexy props', nextProps);
+		this.setState({src: nextProps.src});
+	}
+
+	render() {
+		return (
+			<Pano source={asset(this.state.src)} />
+		)
+	}
+}
+
+export default Canvas;
